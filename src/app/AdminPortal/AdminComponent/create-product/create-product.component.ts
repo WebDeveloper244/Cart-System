41917928dd8f5e7fd2f1f95bce0812faaa86e6b6
WebDevolper1122 <sb2137842@gmail.com>
2022-07-19 07:02:49 +0500

    commit

--- src/app/AdminPortal/AdminComponent/create-product/create-product.component.ts
+++ src/app/AdminPortal/AdminComponent/create-product/create-product.component.ts
@@ -22,14 +22,32 @@ export class CreateProductComponent implements OnInit {
   productFormBuilder(){
     this.productForm = this.formBuilder.group({
       Name:["",[Validators.required,Validators.minLength(4),Validators.maxLength(40),Validators.pattern(/^[A-Za-z]*$/)]],
-      Email:["",[Validators.required,Validators.email]]
+      Email:["",[Validators.required,Validators.email]],
+      Image:['']
     })
   }
 
 
-  submitProductForm(){
-    console.log(this.productForm.value);
-    
+ 
+  getImage(getImageData:any){
+//console.log(event);$event check in target.files
+const _getImage = getImageData.target.files[0];
+this.productForm.get("image").setValue(_getImage);
+console.log(_getImage);
+
+ 
+
+
   }
+;
+submitProductForm(){
+  console.log(this.productForm.value);
+  let  MyFormValue = new FormData();
+  
+  MyFormValue.append('Name',this.productForm.get('Name').value)
+  MyFormValue.append('Email',this.productForm.get('Email').value)
+  MyFormValue.append('Image',this.productForm.get('Image').value)
+  
+}
 
 }
